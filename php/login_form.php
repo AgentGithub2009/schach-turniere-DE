286d617310f50fc400bc10057c94b3e3a4dac563
AgentGithub2009 <125482656+AgentGithub2009@users.noreply.github.com>
2023-03-18 20:23:54 +0100

    Try Login System
    Alpha

---
+++ php/login_form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Schach-Turniere-DE: login form</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <script src='lib/jquery.js'></script>
+    <script src='js/Main.js'></script>
+</head>
+
+<body>
+
+    <div class="form-container">
+
+    <form action="" method="post">
+        <h3>login now</h3>
+        <input type="text" name="username" required placeholder="enter your username">
+        <input type="password" name="password" required placeholder="enter your password">
+        <input type="submit" name="submit" value="login now" class="form-btn">
+        <p>don't have an account? <a href="register_form.php">register now</a></p>
+    </form>
+
+    </div>
+
+</body>
+</html>
---
+++ php/register_form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Schach-Turniere-DE: register form</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <script src='lib/jquery.js'></script>
+    <script src='js/Main.js'></script>
+</head>
+
+<body>
+
+    <div class="form-container">
+
+    <form action="" method="post">
+        <h3>register now</h3>
+        <input type="text" name="username" required placeholder="enter your username">
+        <input type="password" name="password" required placeholder="enter your password">
+        <input type="submit" name="submit" value="register now" class="form-btn">
+        <p>already have an account? <a href="login_form.php">login now</a></p>
+    </form>
+
+    </div>
+
+</body>
+</html>
